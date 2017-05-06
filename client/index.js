779632112df6a3d0c6e387cf61ccc3461003eb41
Michael Foos <darkshelf@gmail.com>
2017-05-06 01:16:24 -0400

    broken sass, comitting what I have before debugging

--- client/index.js
+++ client/index.js
@@ -4,5 +4,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import './scss/App.scss';  
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,21 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.scss$/,
+        include: path.appSrc,
+        loaders: ["style-loader", "css-loader", "sass-loader"],
+        exclude: [
+          /\.html$/,
+          /\.(js|jsx)$/,
+          /\.css$/,
+          /\.json$/,
+          /\.svg$/,
+        ],
+},
     ]
   },
-  
+
   plugins: [HtmlWebpackPluginConfig]
 }
